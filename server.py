fab02f2905e189eb4ac70614cefe6c6e7f4fa77b
Abhishek Kumar <98731119+thekrabhishek@users.noreply.github.com>
2025-01-11 13:26:10 +0530

    gg

--- server.py
+++ server.py
@@ -5,7 +5,38 @@ app = Flask(__name__)
 @app.route('/files', methods=['GET'])
 def get_files():
     files = [
-        {"name": "Chemistry Notes Chapter 1", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Chemistry"},
+        {"name": "Chemistry Notes Unit 1", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Chemistry"},
+        {"name": "Chemistry Notes Unit 2", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Chemistry"},
+        {"name": "Chemistry Notes Unit 3", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Chemistry"},
+        {"name": "Chemistry Notes Unit 4", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Chemistry"},
+        {"name": "Chemistry Notes Unit 5", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Chemistry"},
+
+    
+        {"name": "Chemistry  Unit 1", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Chemistry_pyq"},
+        {"name": "Chemistry  Unit 2", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Chemistry_pyq"},
+        {"name": "Chemistry  Unit 3", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Chemistry_pyq"},
+        {"name": "Chemistry  Unit 4", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Chemistry_pyq"},
+        {"name": "Chemistry  Unit 5", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Chemistry_pyq"},
+
+        
+        {"name": "Physics Unit 1", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Physics_notes"},
+        {"name": "Physics Unit 2", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Physics_notes"},
+        {"name": "Physics Unit 3", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Physics_notes"},
+        {"name": "Physics Unit 4", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Physics_notes"},
+        {"name": "Physics Unit 5", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Physics_notes"},
+
+    
+        {"name": "Physics Unit 1", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Physics_pyq"},
+        {"name": "Physics Unit 2", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Physics_pyq"},
+        {"name": "Physics Unit 3", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Physics_pyq"},
+        {"name": "Physics Unit 4", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Physics_pyq"},
+        {"name": "Physics Unit 5", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Physics_pyq"},
+
+
+
+
+
+
         {"name": "Chemistry Notes Chapter 2", "url": "https://raw.githubusercontent.com/username/repository/branch/path/to/chapter2.pdf", "category": "Chemistry"},
         {"name": "Physics Notes Chapter 1", "url": "https://raw.githubusercontent.com/username/repository/branch/path/to/chapter1.pdf", "category": "Electrical_pyq"},
         {"name": "Physics Notes Chapter 2", "url": "https://raw.githubusercontent.com/username/repository/branch/path/to/chapter2.pdf", "category": "Electrical_notes"},
